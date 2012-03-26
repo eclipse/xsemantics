f1fc352d6fd6cc2a2506427b142b5c74b4fa2eef
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-03-26 16:34:02 +0200

    dep on org.eclipse.xtext.xbase non optional

--- plugins/it.xsemantics.dsl/xtend-gen/it/xsemantics/dsl/generator/GeneratorUtils.java
+++ plugins/it.xsemantics.dsl/xtend-gen/it/xsemantics/dsl/generator/GeneratorUtils.java
@@ -1,22 +1,23 @@
 package it.xsemantics.dsl.generator;
 
-import com.google.common.base.Objects;
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.validation.Issue;
+import org.eclipse.xtext.xbase.lib.BooleanExtensions;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 
 @SuppressWarnings("all")
 public class GeneratorUtils {
-  public static ArrayList<Issue> onlyErrors(final List<Issue> issues) {
+  public static ArrayList<? extends Object> onlyErrors(final List<Issue> issues) {
     final Function1<Issue,Boolean> _function = new Function1<Issue,Boolean>() {
         public Boolean apply(final Issue it) {
           Severity _severity = it.getSeverity();
-          boolean _equals = Objects.equal(_severity, Severity.ERROR);
-          return Boolean.valueOf(_equals);
+          boolean _operator_equals = ObjectExtensions.operator_equals(_severity, Severity.ERROR);
+          return Boolean.valueOf(_operator_equals);
         }
       };
     Iterable<Issue> _filter = IterableExtensions.<Issue>filter(issues, _function);
@@ -25,9 +26,9 @@ public class GeneratorUtils {
   }
   
   public static boolean hasErrors(final List<Issue> issues) {
-    ArrayList<Issue> _onlyErrors = GeneratorUtils.onlyErrors(issues);
+    ArrayList<? extends Object> _onlyErrors = GeneratorUtils.onlyErrors(issues);
     boolean _isEmpty = _onlyErrors.isEmpty();
-    boolean _not = (!_isEmpty);
-    return _not;
+    boolean _operator_not = BooleanExtensions.operator_not(_isEmpty);
+    return _operator_not;
   }
 }
