d39507b127199dbe86f0f837a40fd9b9a0f9ad04
Lorenzo Bettini <bettini@bettini-desk-lucid.(none)>
2012-03-27 17:57:55 +0200

    removed wrong @Override annotations

--- plugins/it.xsemantics.dsl.tests/src/it/xsemantics/dsl/tests/generator/fj/FjActualTypeSystemWrapper.java
+++ plugins/it.xsemantics.dsl.tests/src/it/xsemantics/dsl/tests/generator/fj/FjActualTypeSystemWrapper.java
@@ -18,13 +18,13 @@ import org.eclipse.emf.ecore.EObject;
 public class FjActualTypeSystemWrapper extends FjTypeSystem
 		implements IFjTypeSystem {
 
-	@Override
+	
 	public Result<List<it.xsemantics.example.fj.fj.Class>> superclasses(
 			it.xsemantics.example.fj.fj.Class _Class) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<List<it.xsemantics.example.fj.fj.Class>> superclasses(
 			RuleEnvironment _environment_,
 			RuleApplicationTrace _trace_,
@@ -32,53 +32,53 @@ public class FjActualTypeSystemWrapper extends FjTypeSystem
 		return null;
 	}
 
-	@Override
+	
 	public Result<List<Field>> fields(
 			it.xsemantics.example.fj.fj.Class _Class) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<List<Field>> fields(RuleEnvironment _environment_,
 			RuleApplicationTrace _trace_,
 			it.xsemantics.example.fj.fj.Class _Class) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<List<Method>> methods(
 			it.xsemantics.example.fj.fj.Class _Class) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<Type> typedecl(TypedElement _TypedElement) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<Type> typedecl(RuleEnvironment _environment_,
 			RuleApplicationTrace _trace_, TypedElement _TypedElement) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<Boolean> subclass(Class _Class, Class _Class_1) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<Boolean> subclass(RuleEnvironment _environment_,
 			RuleApplicationTrace _trace_, Class _Class, Class _Class_1) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<Boolean> check(EObject _EObject) {
 		return null;
 	}
 
-	@Override
+	
 	public Result<Boolean> check(RuleEnvironment _environment_,
 			RuleApplicationTrace _trace_, EObject _EObject) {
 		return null;
