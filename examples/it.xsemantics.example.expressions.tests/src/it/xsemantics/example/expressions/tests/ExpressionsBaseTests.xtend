e1f4f83597aeafe9c5280a781e0ba4787e906816
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-04-04 21:54:11 +0200

    System::setProperty("line.separator", "\n") in ExpressionsTests

--- examples/it.xsemantics.example.expressions.tests/src/it/xsemantics/example/expressions/tests/ExpressionsBaseTests.xtend
+++ examples/it.xsemantics.example.expressions.tests/src/it/xsemantics/example/expressions/tests/ExpressionsBaseTests.xtend
@@ -7,6 +7,7 @@ import org.eclipse.xtext.junit4.InjectWith
 import org.eclipse.xtext.junit4.XtextRunner
 import org.junit.Before
 import org.junit.runner.RunWith
+import org.junit.BeforeClass
 
 @RunWith(typeof(XtextRunner))
 @InjectWith(typeof(ExpressionsInjectorProvider))
@@ -15,6 +16,11 @@ class ExpressionsBaseTests {
 	@Inject protected IExpressionsSemantics semantics
 	
 	protected RuleApplicationTrace trace
+	
+	@BeforeClass
+	def static void setNewLine() {
+		System::setProperty("line.separator", "\n")
+	}
 	
 	@Before
 	def void setUp() {
--- examples/it.xsemantics.example.expressions.tests/xtend-gen/it/xsemantics/example/expressions/tests/ExpressionsBaseTests.java
+++ examples/it.xsemantics.example.expressions.tests/xtend-gen/it/xsemantics/example/expressions/tests/ExpressionsBaseTests.java
@@ -7,6 +7,7 @@ import it.xsemantics.runtime.RuleApplicationTrace;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @SuppressWarnings("all")
@@ -17,6 +18,11 @@ public class ExpressionsBaseTests {
   protected IExpressionsSemantics semantics;
   
   protected RuleApplicationTrace trace;
+  
+  @BeforeClass
+  public static void setNewLine() {
+    System.setProperty("line.separator", "\n");
+  }
   
   @Before
   public void setUp() {
