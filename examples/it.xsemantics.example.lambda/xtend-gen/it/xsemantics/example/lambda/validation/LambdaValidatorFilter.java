6549357b7bf89933c46249c41f56b3d8b316fd68
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-04-20 16:56:48 +0200

    regenerated files

--- examples/it.xsemantics.example.lambda/xtend-gen/it/xsemantics/example/lambda/validation/LambdaValidatorFilter.java
+++ examples/it.xsemantics.example.lambda/xtend-gen/it/xsemantics/example/lambda/validation/LambdaValidatorFilter.java
@@ -1,13 +1,11 @@
 package it.xsemantics.example.lambda.validation;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import it.xsemantics.runtime.RuleFailedException;
 import it.xsemantics.runtime.util.TraceUtils;
 import it.xsemantics.runtime.validation.XsemanticsValidatorFilter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 
 @SuppressWarnings("all")
 public class LambdaValidatorFilter extends XsemanticsValidatorFilter {
@@ -15,15 +13,12 @@ public class LambdaValidatorFilter extends XsemanticsValidatorFilter {
   private TraceUtils _traceUtils;
   
   public Iterable<RuleFailedException> filterRuleFailedExceptions(final RuleFailedException e) {
-      RuleFailedException _innermostRuleFailedExceptionWithNodeModelSources = this._traceUtils.innermostRuleFailedExceptionWithNodeModelSources(e);
-      final RuleFailedException inner = _innermostRuleFailedExceptionWithNodeModelSources;
-      boolean _operator_notEquals = ObjectExtensions.operator_notEquals(inner, null);
-      if (_operator_notEquals) {
-        LinkedList<RuleFailedException> _failureAsList = this._traceUtils.failureAsList(inner);
-        return _failureAsList;
-      } else {
-        ArrayList<RuleFailedException> _newArrayList = Lists.<RuleFailedException>newArrayList(e);
-        return _newArrayList;
-      }
+    final RuleFailedException inner = this._traceUtils.innermostRuleFailedExceptionWithNodeModelSources(e);
+    boolean _notEquals = (!Objects.equal(inner, null));
+    if (_notEquals) {
+      return this._traceUtils.failureAsList(inner);
+    } else {
+      return Lists.<RuleFailedException>newArrayList(e);
+    }
   }
 }
