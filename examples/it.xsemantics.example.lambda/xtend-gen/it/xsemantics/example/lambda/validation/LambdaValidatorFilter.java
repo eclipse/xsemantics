f00a4242bc962d01259f6a4cb5c3b25614f6c9db
Lorenzo Bettini <bettini@dsi.unifi.it>
2012-04-20 10:55:59 +0200

    fixed implementation of LambdaValidatorFilter

--- examples/it.xsemantics.example.lambda/xtend-gen/it/xsemantics/example/lambda/validation/LambdaValidatorFilter.java
+++ examples/it.xsemantics.example.lambda/xtend-gen/it/xsemantics/example/lambda/validation/LambdaValidatorFilter.java
@@ -1,10 +1,13 @@
 package it.xsemantics.example.lambda.validation;
 
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import it.xsemantics.runtime.RuleFailedException;
 import it.xsemantics.runtime.util.TraceUtils;
 import it.xsemantics.runtime.validation.XsemanticsValidatorFilter;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 
 @SuppressWarnings("all")
 public class LambdaValidatorFilter extends XsemanticsValidatorFilter {
@@ -12,8 +15,15 @@ public class LambdaValidatorFilter extends XsemanticsValidatorFilter {
   private TraceUtils _traceUtils;
   
   public Iterable<RuleFailedException> filterRuleFailedExceptions(final RuleFailedException e) {
-    RuleFailedException _innermostRuleFailedExceptionWithNodeModelSources = this._traceUtils.innermostRuleFailedExceptionWithNodeModelSources(e);
-    LinkedList<RuleFailedException> _failureAsList = this._traceUtils.failureAsList(_innermostRuleFailedExceptionWithNodeModelSources);
-    return _failureAsList;
+      RuleFailedException _innermostRuleFailedExceptionWithNodeModelSources = this._traceUtils.innermostRuleFailedExceptionWithNodeModelSources(e);
+      final RuleFailedException inner = _innermostRuleFailedExceptionWithNodeModelSources;
+      boolean _operator_notEquals = ObjectExtensions.operator_notEquals(inner, null);
+      if (_operator_notEquals) {
+        LinkedList<RuleFailedException> _failureAsList = this._traceUtils.failureAsList(inner);
+        return _failureAsList;
+      } else {
+        ArrayList<RuleFailedException> _newArrayList = Lists.<RuleFailedException>newArrayList(e);
+        return _newArrayList;
+      }
   }
 }
