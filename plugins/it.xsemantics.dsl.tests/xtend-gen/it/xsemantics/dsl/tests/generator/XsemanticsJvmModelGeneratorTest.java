3e2aac1dd8ab326bfb755c97756abf13e84547f9
Lorenzo Bettini <bettini@dsi.unifi.it>
2013-03-22 12:02:56 +0100

    explicit version override in cquery
    Change-Id: I617b6d052eb1582ed9d381824a576fa8472afa78

--- plugins/it.xsemantics.dsl.tests/xtend-gen/it/xsemantics/dsl/tests/generator/XsemanticsJvmModelGeneratorTest.java
+++ plugins/it.xsemantics.dsl.tests/xtend-gen/it/xsemantics/dsl/tests/generator/XsemanticsJvmModelGeneratorTest.java
@@ -6482,8 +6482,8 @@ public class XsemanticsJvmModelGeneratorTest extends XsemanticsBaseTest {
         }
       };
     this._xsemanticsCompilationTestHelper.compileAll(system, new IAcceptor<Result>() {
-        public void accept(Result arg0) {
-          _function.apply(arg0);
+        public void accept(Result t) {
+          _function.apply(t);
         }
     });
   }
