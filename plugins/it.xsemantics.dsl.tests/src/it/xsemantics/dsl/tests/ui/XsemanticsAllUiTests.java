7127f1da0d683336a6c8e4ab25425eee8f2d8c2a
Lorenzo Bettini <bettini@dsi.unifi.it>
2014-02-10 13:59:04 +0100

    suite for ui tests
    Change-Id: I8e22d88fa49173f647db6320c560b8a8aa2f2509

---
+++ plugins/it.xsemantics.dsl.tests/src/it/xsemantics/dsl/tests/ui/XsemanticsAllUiTests.java
@@ -0,0 +1,14 @@
+package it.xsemantics.dsl.tests.ui;
+
+import it.xsemantics.dsl.tests.ui.contentassist.XsemanticsProposalProviderTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	XsemanticsProposalProviderTest.class
+})
+public class XsemanticsAllUiTests {
+
+}
