02e9bbbf0d5cb0adbcca5c0faef241107c2dc2a3
Lorenzo Bettini <bettini@dsi.unifi.it>
2014-02-27 14:21:03 +0100

    caching of other fj judgments in tests
    Change-Id: I0fa44c3b76b47bfb9c8bff88ebc8fe58de8b7eea

---
+++ tests/it.xsemantics.dsl.tests/src/it/xsemantics/dsl/tests/generator/fj/FjFirstCachedGeneratedTypeSystemTests.xtend
@@ -0,0 +1,12 @@
+package it.xsemantics.dsl.tests.generator.fj;
+
+import it.xsemantics.dsl.tests.generator.fj.common.FjAbstractGeneratedTypeSystemTests
+
+class FjFirstCachedGeneratedTypeSystemTests extends
+		FjAbstractGeneratedTypeSystemTests {
+
+	override fjCustomStandaloneSetupClass() {
+		return FjFirstCachedStandaloneSetup;
+	}
+
+}
--- tests/it.xsemantics.dsl.tests/test-gen/fj_cached_test/it/xsemantics/test/fj/caching/FjFirstCachedTypeSystem.java
+++ tests/it.xsemantics.dsl.tests/test-gen/fj_cached_test/it/xsemantics/test/fj/caching/FjFirstCachedTypeSystem.java
@@ -1,6 +1,8 @@
 package it.xsemantics.test.fj.caching;
 
 import it.xsemantics.example.fj.fj.Expression;
+import it.xsemantics.example.fj.fj.Field;
+import it.xsemantics.example.fj.fj.Method;
 import it.xsemantics.example.fj.fj.Type;
 import it.xsemantics.runtime.ErrorInformation;
 import it.xsemantics.runtime.Result;
@@ -9,11 +11,18 @@ import it.xsemantics.runtime.RuleEnvironment;
 import it.xsemantics.runtime.RuleFailedException;
 import it.xsemantics.runtime.caching.XsemanticsProvider;
 import it.xsemantics.test.fj.first.FjFirstTypeSystem;
+import java.util.List;
 import org.eclipse.xtext.util.PolymorphicDispatcher;
 
 @SuppressWarnings("all")
 public class FjFirstCachedTypeSystem extends FjFirstTypeSystem {
   private PolymorphicDispatcher<Result<Type>> typeDispatcher;
+  
+  private PolymorphicDispatcher<Result<Boolean>> subclassDispatcher;
+  
+  private PolymorphicDispatcher<Result<List<Field>>> fieldsDispatcher;
+  
+  private PolymorphicDispatcher<Result<List<Method>>> methodsDispatcher;
   
   public FjFirstCachedTypeSystem() {
     init();
@@ -24,6 +33,12 @@ public class FjFirstCachedTypeSystem extends FjFirstTypeSystem {
     super.init();
     typeDispatcher = buildPolymorphicDispatcher1(
     	"typeImpl", 3, "|-", ":");
+    subclassDispatcher = buildPolymorphicDispatcher1(
+    	"subclassImpl", 4, "|-", "<|");
+    fieldsDispatcher = buildPolymorphicDispatcher1(
+    	"fieldsImpl", 3, "||-", ">>");
+    methodsDispatcher = buildPolymorphicDispatcher1(
+    	"methodsImpl", 3, "||~", ">>");
   }
   
   @Override
@@ -46,6 +61,83 @@ public class FjFirstCachedTypeSystem extends FjFirstTypeSystem {
   }
   
   @Override
+  public Result<Boolean> subclass(final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    return subclass(new RuleEnvironment(), null, candidate, superclass);
+  }
+  
+  @Override
+  public Result<Boolean> subclass(final RuleEnvironment _environment_, final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    return subclass(_environment_, null, candidate, superclass);
+  }
+  
+  @Override
+  public Result<Boolean> subclass(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    try {
+    	return subclassInternal(_environment_, _trace_, candidate, superclass);
+    } catch (Exception _e_subclass) {
+    	return resultForFailure(_e_subclass);
+    }
+  }
+  
+  @Override
+  public Boolean subclassSucceeded(final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    return subclassSucceeded(new RuleEnvironment(), null, candidate, superclass);
+  }
+  
+  @Override
+  public Boolean subclassSucceeded(final RuleEnvironment _environment_, final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    return subclassSucceeded(_environment_, null, candidate, superclass);
+  }
+  
+  @Override
+  public Boolean subclassSucceeded(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    try {
+    	subclassInternal(_environment_, _trace_, candidate, superclass);
+    	return true;
+    } catch (Exception _e_subclass) {
+    	return false;
+    }
+  }
+  
+  @Override
+  public Result<List<Field>> fields(final it.xsemantics.example.fj.fj.Class cl) {
+    return fields(new RuleEnvironment(), null, cl);
+  }
+  
+  @Override
+  public Result<List<Field>> fields(final RuleEnvironment _environment_, final it.xsemantics.example.fj.fj.Class cl) {
+    return fields(_environment_, null, cl);
+  }
+  
+  @Override
+  public Result<List<Field>> fields(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class cl) {
+    try {
+    	return fieldsInternal(_environment_, _trace_, cl);
+    } catch (Exception _e_fields) {
+    	return resultForFailure(_e_fields);
+    }
+  }
+  
+  @Override
+  public Result<List<Method>> methods(final it.xsemantics.example.fj.fj.Class cl) {
+    return methods(new RuleEnvironment(), null, cl);
+  }
+  
+  @Override
+  public Result<List<Method>> methods(final RuleEnvironment _environment_, final it.xsemantics.example.fj.fj.Class cl) {
+    return methods(_environment_, null, cl);
+  }
+  
+  @Override
+  public Result<List<Method>> methods(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class cl) {
+    try {
+    	return methodsInternal(_environment_, _trace_, cl);
+    } catch (Exception _e_methods) {
+    	return resultForFailure(_e_methods);
+    }
+  }
+  
+  @Override
   protected Result<Type> typeInternal(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final Expression expression) {
     return getFromCache("typeInternal", _environment_, _trace_,
     	new XsemanticsProvider<Result<Type>>(_environment_, _trace_) {
@@ -63,6 +155,69 @@ public class FjFirstCachedTypeSystem extends FjFirstTypeSystem {
   
   @Override
   protected void typeThrowException(final String _error, final String _issue, final Exception _ex, final Expression expression, final ErrorInformation[] _errorInformations) throws RuleFailedException {
+    throwRuleFailedException(_error, _issue, _ex, _errorInformations);
+  }
+  
+  @Override
+  protected Result<Boolean> subclassInternal(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass) {
+    return getFromCache("subclassInternal", _environment_, _trace_,
+    	new XsemanticsProvider<Result<Boolean>>(_environment_, _trace_) {
+    		public Result<Boolean> doGet() {
+    			try {
+    				checkParamsNotNull(candidate, superclass);
+    				return subclassDispatcher.invoke(_environment_, _trace_, candidate, superclass);
+    			} catch (Exception _e_subclass) {
+    				sneakyThrowRuleFailedException(_e_subclass);
+    				return null;
+    			}
+    		}
+    	}, candidate, superclass);
+  }
+  
+  @Override
+  protected void subclassThrowException(final String _error, final String _issue, final Exception _ex, final it.xsemantics.example.fj.fj.Class candidate, final it.xsemantics.example.fj.fj.Class superclass, final ErrorInformation[] _errorInformations) throws RuleFailedException {
+    throwRuleFailedException(_error, _issue, _ex, _errorInformations);
+  }
+  
+  @Override
+  protected Result<List<Field>> fieldsInternal(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class cl) {
+    return getFromCache("fieldsInternal", _environment_, _trace_,
+    	new XsemanticsProvider<Result<List<Field>>>(_environment_, _trace_) {
+    		public Result<List<Field>> doGet() {
+    			try {
+    				checkParamsNotNull(cl);
+    				return fieldsDispatcher.invoke(_environment_, _trace_, cl);
+    			} catch (Exception _e_fields) {
+    				sneakyThrowRuleFailedException(_e_fields);
+    				return null;
+    			}
+    		}
+    	}, cl);
+  }
+  
+  @Override
+  protected void fieldsThrowException(final String _error, final String _issue, final Exception _ex, final it.xsemantics.example.fj.fj.Class cl, final ErrorInformation[] _errorInformations) throws RuleFailedException {
+    throwRuleFailedException(_error, _issue, _ex, _errorInformations);
+  }
+  
+  @Override
+  protected Result<List<Method>> methodsInternal(final RuleEnvironment _environment_, final RuleApplicationTrace _trace_, final it.xsemantics.example.fj.fj.Class cl) {
+    return getFromCache("methodsInternal", _environment_, _trace_,
+    	new XsemanticsProvider<Result<List<Method>>>(_environment_, _trace_) {
+    		public Result<List<Method>> doGet() {
+    			try {
+    				checkParamsNotNull(cl);
+    				return methodsDispatcher.invoke(_environment_, _trace_, cl);
+    			} catch (Exception _e_methods) {
+    				sneakyThrowRuleFailedException(_e_methods);
+    				return null;
+    			}
+    		}
+    	}, cl);
+  }
+  
+  @Override
+  protected void methodsThrowException(final String _error, final String _issue, final Exception _ex, final it.xsemantics.example.fj.fj.Class cl, final ErrorInformation[] _errorInformations) throws RuleFailedException {
     throwRuleFailedException(_error, _issue, _ex, _errorInformations);
   }
 }
