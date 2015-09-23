f4dc1317d29af7f89340b1600cd17122ea4df1ab
Lorenzo Bettini <bettini@dsi.unifi.it>
2015-09-23 20:21:37 +0200

    fixed comment in FJ cached system
    Change-Id: I37eedfc579d4cc119f013ac6ffc02cb7c4175f46

--- examples/it.xsemantics.example.fjcached/xsemantics-gen/it/xsemantics/example/fjcached/typing/validation/FjCachedTypeSystemValidator.java
+++ examples/it.xsemantics.example.fjcached/xsemantics-gen/it/xsemantics/example/fjcached/typing/validation/FjCachedTypeSystemValidator.java
@@ -5,8 +5,7 @@ import it.xsemantics.example.fj.typing.validation.FjTypeSystemValidator;
 import it.xsemantics.example.fjcached.typing.FjCachedTypeSystem;
 
 /**
- * A version of the FJ system where type computation and
- * checking are separated.
+ * A version of the FJ system where caching is used.
  */
 @SuppressWarnings("all")
 public class FjCachedTypeSystemValidator extends FjTypeSystemValidator {
