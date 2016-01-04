3793dc18c01364d9ab2a69b7f5c8dce5a13a3751
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-01-04 14:24:40 +0100

    67: Build on Travis MacOSX only on pull requests
    Task-Url: https://github.com/LorenzoBettini/xsemantics/issues/issue/67

---
+++ travis_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+		echo "Build on MacOSX: Pull Request"
+		mvn -f releng/it.xsemantics.releng/pom.xml clean verify -U -Dfindbugs.skip=true
+	else
+		echo "Skipping build on MacOSX for standard commit"
+	fi
+else
+	echo "Build on Linux"
+	mvn -f releng/it.xsemantics.releng/pom.xml clean verify -U -Dfindbugs.skip=true -Pjacoco-report coveralls:report
+fi 
